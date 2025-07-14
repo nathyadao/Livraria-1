d0e9c32696acd1fa2b4e4140d60e7a750a11aa29
nathyadao <nathalycarolineadaoo@gmail.com>
2025-07-14 15:41:58 -0300

    #25 feat: mostrando o total do item na listagem de compras.

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,14 +1,16 @@
 
-from rest_framework.serializers import CharField, ModelSerializer
+from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
 
 from core.models import Compra, ItensCompra
 
 class ItensCompraSerializer(ModelSerializer):
+    total = SerializerMethodField()
+    def get_total(self, instance):
+        return instance.livro.preco * instance.quantidade
     class Meta:
         model = ItensCompra
-        fields = ('id', 'livro', 'quantidade')
-        depth = 2
-        
+        fields = ('livro', 'quantidade', 'total')
+        depth = 1
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.e-mail', read_only=True) 
     status = CharField(source='get_status_display', read_only=True)
